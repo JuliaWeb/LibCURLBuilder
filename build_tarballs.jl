4dd0e486f84bb3e421f1cac89735f7d847eed687
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-10-20 11:52:28 +0800

    update deps: mbedtls
    ref: https://github.com/JuliaWeb/LibCURL.jl/pull/56

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -54,7 +54,7 @@ products(prefix) = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.2/build_Zlib.v1.2.11.jl",
-    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.11/build_MbedTLS.v1.0.0.jl"
+    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.16.0/build_MbedTLS.v2.13.1.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
