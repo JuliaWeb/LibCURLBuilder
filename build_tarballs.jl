471f19dc72e95460049d3ed49255e5ad9415a379
Fernando Chorney <djsbx@djsbx.com>
2019-03-14 16:41:10 -0500

    Update CURL, MbedTLS, and Zlib (#12)
    * Update CURL, MbedTLS, and Zlib
    
    - CURL: 7.61.0 -> 7.64.0
    - MbedTLS: 0.11.0 -> 0.17.0
    - Zlib: 1.0.2 -> 1.0.3
    - FreeBSD Doesn't seem to want to compile anymore
    
    * Need to use v0.6.6 of MbedTLS for BinaryBuilder
    
    - If we use 0.6.7 then we get this error https://github.com/JuliaWeb/MbedTLS.jl/issues/193
    
    * Add certpath to configure. Add default LDFLAGS
    
    * Pin BinaryBuilder to 0.1.4 instead of using master
    
    * Need to jump to 1.0 for the build
    
    * add Cmment about MbedTLS, don't pin BinaryBuilder
    
    * Remove the --with-ca-bundle flag

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,60 +3,70 @@
 using BinaryBuilder
 
 name = "LibCURL"
-version = v"7.61.0"
+version = v"7.64.0"
 
 # Collection of sources required to build LibCURL
 sources = [
-    "https://curl.haxx.se/download/curl-7.61.0.tar.gz" =>
-    "64141f0db4945268a21b490d58806b97c615d3d0c75bf8c335bbe0efd13b45b5",
-
+    "https://curl.haxx.se/download/curl-7.64.0.tar.gz" =>
+    "cb90d2eb74d4e358c1ed1489f8e3af96b50ea4374ad71f143fa4595e998d81b5",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/curl-7.61.0
-./configure --prefix=$prefix --host=$target --with-mbedtls --without-ssl --disable-manual
+cd $WORKSPACE/srcdir/curl-7.64.0
+
+# Configure and build
+./configure \
+    --prefix=$prefix \
+    --host=$target \
+    --with-mbedtls \
+    --without-ssl \
+    --disable-manual
+
 if [[ $target == *-w64-mingw32 ]]; then
-    LDFLAGS="-L$prefix/bin"
+    LDFLAGS="$LDFLAGS -L$prefix/bin"
 elif [[ $target == x86_64-apple-darwin14 ]]; then
-    LDFLAGS="-L$prefix/lib -Wl,-rpath,$prefix/lib"
+    LDFLAGS="$LDFLAGS -L$prefix/lib -Wl,-rpath,$prefix/lib"
 else
-    LDFLAGS="-L$prefix/lib -Wl,-rpath-link,$prefix/lib"
+    LDFLAGS="$LDFLAGS -L$prefix/lib -Wl,-rpath-link,$prefix/lib"
 fi
 make -j${nproc} LDFLAGS="$LDFLAGS"
 make install-exec
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
-    Linux(:powerpc64le, :glibc),
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
     MacOS(:x86_64),
-    FreeBSD(:x86_64),
     Windows(:i686),
     Windows(:x86_64),
 ]
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix, "libcurl", :libcurl),
+    LibraryProduct(prefix, "libcurl", :libcurl)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.2/build_Zlib.v1.2.11.jl",
-    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.11/build_MbedTLS.v1.0.0.jl"
+    string(
+        "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/",
+        "v0.17.0/build_MbedTLS.v2.16.0.jl",
+    ),
+    string(
+        "https://github.com/bicycle1885/ZlibBuilder/releases/download/",
+        "v1.0.3/build_Zlib.v1.2.11.jl",
+    ),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
