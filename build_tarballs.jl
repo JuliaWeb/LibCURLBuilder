bfefc812c642ed734f765c51ad01c6e730345748
Curtis Vogt <curtis.vogt@gmail.com>
2019-05-16 14:06:54 -0500

    Re-add FreeBSD

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -47,6 +47,7 @@ platforms = [
     Linux(:aarch64, libc=:musl),
     Linux(:armv7l, libc=:musl, call_abi=:eabihf),
     MacOS(:x86_64),
+    FreeBSD(:x86_64),
     Windows(:i686),
     Windows(:x86_64),
 ]
